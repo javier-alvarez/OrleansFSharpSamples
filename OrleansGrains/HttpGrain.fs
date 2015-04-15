28dd16752ba6180cd3b4840d44e3fe1621026e91
Javier Alvarez-Valle <jaalvare@microsoft.com>
2015-04-15 13:21:11 +0100

    Fix packages and add exception in grain

--- OrleansGrains/HttpGrain.fs
+++ OrleansGrains/HttpGrain.fs
@@ -9,9 +9,12 @@ open FSharpx.Task
 
 type HttpGrain() = 
     inherit Orleans.Grain()
+    let mutable counter: int = 0
     interface IHttpGrain with
         override this.Get(uri:Uri) =
             let task = TaskBuilder(scheduler = TaskScheduler.Current)
+            counter <- counter + 1
+            if (counter % 10 = 0) then raise (System.Exception("Multiple of 10!"))
             task {
                 let httpClient = new HttpClient()
                 let! response = httpClient.GetAsync(uri)

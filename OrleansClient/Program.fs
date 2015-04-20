8d0982e10b2277fe1b16e67b2c22712173954f0e
Javier Alvarez-Valle <jaalvare@microsoft.com>
2015-04-20 18:48:36 +0100

    Add exception handling

--- OrleansClient/Program.fs
+++ OrleansClient/Program.fs
@@ -1,16 +1,21 @@
 ﻿open System
+open System.Threading.Tasks
+open FSharpx.Task
 
 [<EntryPoint>]
 let main argv = 
 
     Orleans.GrainClient.Initialize("ClientConfiguration.xml")
     let grain = OrleansGrainsInterfaces.HttpGrainFactory.GetGrain(1L)
-
+    let task = TaskBuilder(scheduler = TaskScheduler.Current)
     while true do 
-        async{
-            let! statusCode = (grain.Get(Uri("http://www.bing.com"))) |> Async.AwaitTask 
-            printfn "%s Bing responded %s" (DateTime.Now.ToString("O")) (statusCode.ToString())
-        }|> Async.RunSynchronously
+        try
+            task{
+                let! statusCode = grain.Get(Uri("http://www.bing.com"))
+                printfn "%s Bing responded %s" (DateTime.Now.ToString("O")) (statusCode.ToString())
+            } |> Task.WaitAll
+        with 
+        | _ -> printfn "hello"
 
 
     0 // return an integer exit code

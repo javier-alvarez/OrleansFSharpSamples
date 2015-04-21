5e7470d9cced5d4589504a60cad6fc2cd10cb89c
Javier Alvarez-Valle <jaalvare@microsoft.com>
2015-04-21 21:39:55 +0100

    Add test to verify that the orleans scheduler is used

--- OrleansGrains/HttpGrain.fs
+++ OrleansGrains/HttpGrain.fs
@@ -7,6 +7,13 @@ open OrleansGrainsInterfaces
 open System.Net.Http
 open FSharpx.Task
 
+let Ignore (task:Task) =
+      let continuation (t : Task) : unit =
+          if t.IsFaulted 
+          then raise t.Exception
+          else ()
+      task.ContinueWith continuation
+
 type HttpGrain() = 
     inherit Orleans.Grain()
     let mutable counter: int = 0
@@ -16,6 +23,17 @@ type HttpGrain() =
             counter <- counter + 1
             if (counter % 10 = 0) then raise (System.Exception("Multiple of 10!"))
             task {
+
+                let ts1 = TaskScheduler.Current; // Grabs the Orleans task scheduler
+                assert ts1.GetType().Namespace.StartsWith("Orleans")
+                let! _ = Task.Delay(100) |> Ignore
+
+                // check the scheduler
+                let ts2 = TaskScheduler.Current; // Grabs the Orleans task scheduler
+                assert ts2.GetType().Namespace.StartsWith("Orleans")
+
+                assert (ts1=ts2)
+
                 let httpClient = new HttpClient()
                 let! response = httpClient.GetAsync(uri)
                 return response.StatusCode
